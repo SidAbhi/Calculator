c5681deefd189a6f71859ba1c03d637f32a4b340
SidAbhi <megamancodenamezerobytes@gmail.com>
2020-10-05 12:40:16 +0700

    Input completed

--- calc.js
+++ calc.js
@@ -1,50 +1,100 @@
-const numB = document.querySelector('#numB');
-const numA = document.querySelector('#numA');
+const calcBox = document.querySelector('#calc');
 const showOp = document.querySelector('.showOp');
 const tempRslt = document.querySelector('#result');
-let numValA = 0;
-let numValB = 0;
+let num = [0, 0];
+let rsltVal = 0;
+let overwrite = true;
 const btnNum = [...document.querySelectorAll('.Num')];
 const btnOp = [...document.querySelectorAll('.Op')];
 let operator = '';
 
-function inputNum() {
-    val = document.querySelector(`#${this.id}`).dataset.value;
-    if(numB.textContent === '0') {
-        numB.textContent = val;
-    } else if(numB.textContent.length < 16){
-        numB.textContent = `${numB.textContent}${val}`;
-    };
-    numValB = parseInt(numB.textContent);
-    console.log(numValB);
-}
 
-function show() {
-    if(operator === 'plus') {
-        showOp.textContent = `+`;
-    } else if(operator === 'neg') {
-        showOp.textContent = `-`;
-    } else if(operator === 'mult') {
-        showOp.textContent = `x`;
-    } else if(operator === 'div') {
-        showOp.textContent = `/`;
+function clear() {
+    calcBox.textContent = '0';
+    num = [0, 0];
+    rsltVal = 0;
+    operator = '';
+    overwrite = true;
+};
+
+function valExt() {
+    str = calcBox.textContent.replace(/[\slog()]/g,'');
+    num = str.split(/[-+x^√\/]/);
+    for (i=0; i<num.length; i++) {
+        num[i] = parseInt(num[i]);
     };
 };
 
-function result() {
-    numB.textContent = `0`;
-    numValA = numValB;
-    operator = document.querySelector(`#${this.id}`).id;
-    numA.textContent = `${numValB}`;
-    console.log(operator);
+function calc() {
+    valExt();
 
-    
+};
+
+function del() {
+    if (Number.isNaN(num[1]) || operator === 'log') {
+        calcBox.textContent = calcBox.textContent.replace(/[-+x^√\/\slog()]/g,'');
+    } else {
+        calcBox.textContent = calcBox.textContent.slice(0, -1);
+    };
+    valExt();
 }
 
-btnNum.forEach(num => {
-    num.addEventListener('click', inputNum);
+function inputNum() {
+    nmpad = document.querySelector(`#${this.id}`).dataset.val;
+    if(overwrite === true) {
+        calcBox.textContent = `${nmpad}`;
+        overwrite = false;
+    } else if(operator === 'log'){
+        valExt();
+        calcBox.textContent = `log(${num[0]}${nmpad})`
+    } else {
+        calcBox.textContent = `${calcBox.textContent}${nmpad}`;
+    }
+    valExt();
+};
+
+function inputOp() {
+    overwrite = false;
+    oppad = document.querySelector(`#${this.id}`).id;
+    opc = calcBox.textContent.replace(/[^-+x^√\/l]/g,"").length;
+    if(oppad === 'plus') {
+        calcBox.textContent = `${calcBox.textContent} + `;
+        operator = oppad;
+    } else if(oppad === 'neg') {
+        calcBox.textContent = `${calcBox.textContent} - `;
+        operator = oppad;
+    } else if(oppad === 'mult') {
+        calcBox.textContent = `${calcBox.textContent} x `;
+        operator = oppad;
+    } else if(oppad === 'div') {
+        calcBox.textContent = `${calcBox.textContent} / `;
+        operator = oppad;
+    } else if(oppad === 'sqrt') {
+        calcBox.textContent = `${calcBox.textContent} √ `;
+        operator = oppad;
+    } else if(oppad === 'pwr') {
+        calcBox.textContent = `${calcBox.textContent} ^ `;
+        operator = oppad;
+    } else if(oppad === 'log') {
+        calcBox.textContent = `log(${calcBox.textContent})`;
+        operator = oppad;
+    } else if(oppad === 'clear'){
+        clear();
+    } else if(oppad === 'del') {
+        valExt();
+        del();
+        if(calcBox.textContent === '0') {
+            overwrite = true;
+        };
+    } else if(oppad === 'eql') {
+        return x;
+    }
+};
+
+btnNum.forEach(val => {
+    val.addEventListener('click', inputNum);
 });
 
 btnOp.forEach(op => {
-    op.addEventListener('click', result);
+    op.addEventListener('click', inputOp)
 });
